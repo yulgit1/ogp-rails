43e0b6e9ceae1a447531b8193e4d94743f6b0672
rnz0 <renzo.sanchez.silva@gmail.com>
2012-07-11 15:23:55 -0700

    Adding wms proxy configuration for this institution dynamically depending on environment

--- config/initializers/ogp_config.rb
+++ config/initializers/ogp_config.rb
@@ -1,6 +1,8 @@
 # OpenGeoportal specific configuration
 solr_yaml = YAML.load_file(File.expand_path(File.join(File.dirname(__FILE__), '..', 'environments', 'solr.yml')))[Rails.env]
+proxy_yaml = YAML.load_file(File.expand_path(File.join(File.dirname(__FILE__), '..', 'environments', 'proxy.yml')))[Rails.env]
 ogp_yaml = YAML.load_file(File.expand_path(File.join(File.dirname(__FILE__), '..', 'environments',  'ogp.yml')))
+
 
 OgpRails::Application.config.solr_url = solr_yaml['serviceProxyAddress'] || solr_yaml['serviceAddress']
 OgpRails::Application.config.ogpConfig = {
@@ -8,3 +10,5 @@ OgpRails::Application.config.ogpConfig = {
     :search => solr_yaml
   })
 }
+
+OgpRails::Application.config.ogpConfig[:config]['institutions']['Stanford']['proxy'] = proxy_yaml 
