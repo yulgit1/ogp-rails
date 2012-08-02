f745c940d9d4af4f3e1daf7e9e17de3985862dcd
rnz0 <renzo.sanchez.silva@gmail.com>
2012-08-02 09:49:25 -0700

    deleting useless php file

--- public/getImage/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-# Request URL:http://geoportal-dev.stanford.edu/getImage
-# Request Method:POST
-# Data: 
-# {"layers":[],"format":"image/png","bbox":"-17121894.333496,-16084796.733867,20565841.079434,10527519.030196","srs":"EPSG:900913","width":963,"height":680}
-
-
-?>
